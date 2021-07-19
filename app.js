a17b1989dfc87656933068117f52cfecf48e553b
Deffelovers <another.n4krs@gmail.com>
2021-07-19 19:31:24 +0700

    ui-web-update#01

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
-
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = process.env.PORT || 2405;
-//
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'web'))
-
-app.get('/', (req, res) =>{
-    res.render('index.ejs')
-})
-
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: {origin: '*'}})
+const port = process.env.PORT || 2400
 app.use('/config', express.static(path.join(__dirname, 'web')))
 
-// app.get('/tes/:text', (req, res) => {
-//     let text = req.params.text
-//     res.send(`Text: ${text}!`)
-// })
+
+app.get('/', (req, res) =>{
+    res.sendFile('index.html', {root: 'web'})
+})
 
 app.get('/*', (req, res) =>{
     res.redirect('/')
 })
 
-app.listen(port, () =>{
-    console.log('[notHUMMAN] Virtual web: http://localhost:2405' )
+server.listen(port, () =>{
+    console.log('Server start on: ' + port)
 })
-
