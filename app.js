73789b3bd45593017fa1ff8ad45a77b68ac71c03
Deffelovers <another.n4krs@gmail.com>
2021-07-25 01:08:05 +0700

    update#03

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({extended: false}))
 app.use('/config', express.static(path.join(__dirname, 'web')))
 app.use('/favicon.ico', express.static(path.join(__dirname, 'web/favicon.ico')))
 // socket.io //
-io.on('connection', (socket) =>{
-    console.log(socket.handshake.headers.cookie)
+io.on('connection', async(socket) =>{
+    console.log(socket.id)
     socket.on('QR', async(req) =>{
         const proc = (req.qr == '') ? false : qr = require('qr-image').imageSync(req.qr, {type: 'png'}).toString('base64')
         socket.emit('QR', proc)
@@ -27,7 +27,26 @@ app.get('/', (req, res) =>{
 })
 
 app.get('/mal', (req, res) =>{
-    res.render('mal.ejs')
+    require('request')('https://myanimelist.net/animelist/Deffelovers_', (error, response, html) =>{
+        if(!error && response.statusCode == 200){
+            const $ = require('cheerio').load(html)
+            const mal_map = JSON.parse($('table').attr('data-items')).map(el =>{
+                return{
+                    id: el.anime_id,
+                    score: el.score,
+                    url: el.anime_url,
+                    title: el.anime_title,
+                    eps: el.anime_num_episodes,
+                    eps_watch: el.num_watched_episodes,
+                    thumb: `https://cdn.myanimelist.net/images/${el.anime_image_path.split('/')[6]}/${el.anime_image_path.split('/')[7]}/${el.anime_image_path.split('/')[8]}`,
+                    finish_date: el.finish_date_string,
+                    end_date: el.anime_end_date_string,
+                    media_type: el.anime_media_type_string+', '+el.anime_mpaa_rating_string ,
+                }
+            })
+            res.render('mal.ejs', {data: mal_map})
+        }else console.log(error)
+    })
 })
 
 app.get('/projects', (req, res) =>{
