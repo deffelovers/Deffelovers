bec3ce10e0137487d5f7d4f5d5d334562464fd7f
Deffelovers <another.n4krs@gmail.com>
2021-07-24 16:19:39 +0700

    update#2

--- app.js
+++ app.js
@@ -11,15 +11,27 @@ app.use('/config', express.static(path.join(__dirname, 'web')))
 app.use('/favicon.ico', express.static(path.join(__dirname, 'web/favicon.ico')))
 // socket.io //
 io.on('connection', (socket) =>{
-    console.log('user connected!: ' + socket.id)
+    console.log(socket.handshake.headers.cookie)
     socket.on('QR', async(req) =>{
         const proc = (req.qr == '') ? false : qr = require('qr-image').imageSync(req.qr, {type: 'png'}).toString('base64')
         socket.emit('QR', proc)
+    })
+    socket.on('MAL', async(req) =>{
+        const mal_d = (req.mal == '') ? false : await (axios = require('axios').get(`https://myanimelist.net/search/prefix.json?type=anime&keyword=${req.mal}`).then((res) => {return {success: true, data: res.data.categories[0].items}}).catch((e) =>{return{succes: false, e}}))
+        socket.emit('MAL', mal_d)
     })
 })
 // web services //
 app.get('/', (req, res) =>{
     res.render('index.ejs')
+})
+
+app.get('/mal', (req, res) =>{
+    res.render('mal.ejs')
+})
+
+app.get('/projects', (req, res) =>{
+    res.render('projects.ejs')
 })
 
 app.get('/tools', (req, res) =>{
---
+++ web/locJS/tls-script.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+    // socket //
+    const socket = io("localhost:2400");
+    // feature 1 //
+    socket.on('QR', (res) =>{(res != false) ? document.querySelector('.cd-ct img').setAttribute('src', `data:image/png;base64,${res}`) : document.querySelector('.cd-ct img').setAttribute('src', 'null');(res != false) ? document.querySelector('.cd-ct a#dl').setAttribute('href', `data:image/png;base64,${res}`) || document.querySelector('.cd-ct a#dl').classList.remove('disabled') : document.querySelector('.cd-ct a#dl').classList.add('disabled'); document.querySelector('.input-group input#qr-input').value = document.querySelector('.input-group input#qr-input').defaultValue;});
+    $('.input-group button#qr-btn').click(function(){socket.emit('QR', {qr: document.querySelector('.input-group input#qr-input').value});});
+    // feature 2 //
+    socket.on('MAL', (res) =>{
+        // const mal_p = document.querySelector('.row div#mal-r');
+        if(res != false){
+            const ch = (res.success == true) ? res.data : res.e
+            console.log(ch)
+        }else{
+            // document.querySelector('.row div#mal-r').innerHTML = '<hr><div id="ntf" class="text-muted text-center" style="font-size: 14px;"><i>Input kosong!.</i></div><hr>'
+        }
+        $(res).ready(function(){
+            document.querySelector('.mb-3 input#mal-s').value = document.querySelector('.mb-3 input#mal-s').defaultValue;
+            document.querySelector('.col button#mal-btn0').classList.toggle('d-none');
+            document.querySelector('.col button#mal-btn1').classList.toggle('d-none');    
+        })
+    })
+    $('.col button#mal-btn0').click(function(){
+        socket.emit('MAL', {mal: document.querySelector('.mb-3 input#mal-s').value});
+        document.querySelector('.col button#mal-btn0').classList.toggle('d-none');
+        document.querySelector('.col button#mal-btn1').classList.toggle('d-none');
+    })
+})
