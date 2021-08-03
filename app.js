03ee2881d633dae5d29e27d07c929eb25b8b8ab8
Deffelovers <another.n4krs@gmail.com>
2021-08-03 21:02:52 +0700

    update#7.2

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, { cors: { origin: '*' } })
 const port = process.env.PORT || 2400
 // 
 app.set('view engine', 'ejs')
@@ -10,19 +9,6 @@ app.set('views', path.join(__dirname, 'web'))
 app.use(express.urlencoded({ extended: false }))
 app.use('/root', express.static(path.join(__dirname, 'web/root')))
 app.use('/favicon.ico', express.static(path.join(__dirname, 'web/favicon.ico')))
-// socket.io //
-io.on('connection', (socket) => {
-  console.log('user connected!: ' + socket.id)
-  socket.on('QR', async (req) => {
-    const proc = (req.qr == '') ? false : qr = require('qr-image').imageSync(req.qr, { type: 'png' }).toString('base64')
-    socket.emit('QR', proc)
-  })
-  socket.on('MAL', async (req) => {
-    const mal_d = (req.mal == '') ? false : await (axios = require('axios').get(`https://myanimelist.net/search/prefix.json?type=anime&keyword=${req.mal}`).then((res) => { return { succes: true, data: res.data.categories[0].items } }).catch((e) => { return { succes: false, e } }))
-    socket.emit('MAL', mal_d)
-  })
-})
-
 // web services //
 app.get('/', (req, res) => {
   res.render('index.ejs')
@@ -72,20 +58,23 @@ app.get('/api/v1/qr', (req, res) => {
 })
 
 app.get('/api/v1/mal', async (req, res) => {
-  const mal = (req.query.search == undefined || req.query.search == '') ? { succes: false, msg: 'Bad Request.' } : {
-    success: true, note: 'Data ini di ambil dari: https://www.myanimelist.net', data: await require('axios').get(`https://myanimelist.net/search/prefix.json?type=anime&keyword=${req.query.search}`).then(res => res.data.categories[0].items.map(e => {
-      return {
-        id: e.id,
-        type: e.payload.media_type,
-        score: e.payload.score,
-        title: e.name,
-        aired: e.payload.aired,
-        url: e.url,
-        thumb: `https://cdn.myanimelist.net/images/${e.image_url.split('/')[6]}/${e.image_url.split('/')[7]}/${e.image_url.split('/')[8]}`,
-      }
-    })).catch(er => er.data)
-  }
-  res.send(mal)
+  req.query.search == undefined || req.query.search == '' ? res.send({ succes: false, msg: 'Input tidak valid!!.' }) :
+  require('request')(`https://myanimelist.net/search/prefix.json?type=anime&keyword=${req.query.search}`, (err, response, html) => {
+    if (!err && response.statusCode == 200){
+      const mal_s = html.map(e =>{
+        return {
+          id: e.id,
+          type: e.payload.media_type,
+          score: e.payload.score,
+          title: e.name,
+          aired: e.payload.aired,
+          url: e.url,
+          thumb: `https://cdn.myanimelist.net/images/${e.image_url.split('/')[6]}/${e.image_url.split('/')[7]}/${e.image_url.split('/')[8]}`,
+        }
+      })
+      res.send({ succes: false, data: mal_s })
+    }else res.send({ succes: false, msg:  `Gagal menghubungkan ke server ${err.hostname}` })
+  })
 })
 
 app.get('/*', (req, res) => {
