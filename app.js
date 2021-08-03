3ba80ca46122db6506689cef4408d9e6cecca866
Deffelovers <another.n4krs@gmail.com>
2021-08-03 09:18:39 +0700

    update#7.1

--- app.js
+++ app.js
@@ -2,95 +2,96 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors: {origin: '*'}})
+const io = require('socket.io')(server, { cors: { origin: '*' } })
 const port = process.env.PORT || 2400
 // 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'web'))
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use('/root', express.static(path.join(__dirname, 'web/root')))
 app.use('/favicon.ico', express.static(path.join(__dirname, 'web/favicon.ico')))
 // socket.io //
-io.on('connection', (socket) =>{
-    console.log('user connected!: ' + socket.id)
-    socket.on('QR', async(req) =>{
-        const proc = (req.qr == '') ? false : qr = require('qr-image').imageSync(req.qr, {type: 'png'}).toString('base64')
-        socket.emit('QR', proc)
-    })
-    socket.on('MAL', async(req) =>{
-        const mal_d = (req.mal == '') ? false : await (axios = require('axios').get(`https://myanimelist.net/search/prefix.json?type=anime&keyword=${req.mal}`).then((res) => {return {succes: true, data: res.data.categories[0].items}}).catch((e) =>{return{succes: false, e}}))
-        socket.emit('MAL', mal_d)
-    })
+io.on('connection', (socket) => {
+  console.log('user connected!: ' + socket.id)
+  socket.on('QR', async (req) => {
+    const proc = (req.qr == '') ? false : qr = require('qr-image').imageSync(req.qr, { type: 'png' }).toString('base64')
+    socket.emit('QR', proc)
+  })
+  socket.on('MAL', async (req) => {
+    const mal_d = (req.mal == '') ? false : await (axios = require('axios').get(`https://myanimelist.net/search/prefix.json?type=anime&keyword=${req.mal}`).then((res) => { return { succes: true, data: res.data.categories[0].items } }).catch((e) => { return { succes: false, e } }))
+    socket.emit('MAL', mal_d)
+  })
 })
 
 // web services //
-app.get('/', (req, res) =>{
-    res.render('index.ejs')
+app.get('/', (req, res) => {
+  res.render('index.ejs')
 })
 
-app.get('/tools', (req, res) =>{
-    res.render('tools.ejs')
+app.get('/tools', (req, res) => {
+  res.render('tools.ejs')
 })
 
-app.get('/mal', (req, res) =>{
-    require('request')('https://myanimelist.net/animelist/deffelovers_', (err, response, html) =>{
-        if(!err && response.statusCode == 200){
-            const mal_d = JSON.parse(require('cheerio').load(html)('table').attr('data-items')).map(e =>{
-                return {
-                    id: e.anime_id,
-                    score: e.score,
-                    url: e.anime_url,
-                    title: e.anime_title,
-                    eps: e.anime_num_episodes,
-                    end_date: e.anime_end_date_string,
-                    eps_watch: e.num_watched_episodes,
-                    finish_date: e.finish_date_string,
-                    type: e.anime_media_type_string + ', ' + e.anime_mpaa_rating_string,
-                    thumb: `https://cdn.myanimelist.net/images/${e.anime_image_path.split('/')[6]}/${e.anime_image_path.split('/')[7]}/${e.anime_image_path.split('/')[8]}`,
-                }
-            })
-            res.render('mal.ejs', {success: true, data: mal_d})
-        }else res.render('mal.ejs', {success: false, data: err})
-    })
+app.get('/mal', (req, res) => {
+  require('request')('https://myanimelist.net/animelist/deffelovers_', (err, response, html) => {
+    if (!err && response.statusCode == 200) {
+      const mal_d = JSON.parse(require('cheerio').load(html)('table').attr('data-items')).map(e => {
+        return {
+          id: e.anime_id,
+          score: e.score,
+          url: e.anime_url,
+          title: e.anime_title,
+          eps: e.anime_num_episodes,
+          end_date: e.anime_end_date_string,
+          eps_watch: e.num_watched_episodes,
+          finish_date: e.finish_date_string,
+          type: e.anime_media_type_string + ', ' + e.anime_mpaa_rating_string,
+          thumb: `https://cdn.myanimelist.net/images/${e.anime_image_path.split('/')[6]}/${e.anime_image_path.split('/')[7]}/${e.anime_image_path.split('/')[8]}`,
+        }
+      })
+      res.render('mal.ejs', { success: true, data: mal_d })
+    } else res.render('mal.ejs', { success: false, data: err })
+  })
 })
 
-app.get('/projects', (req, res) =>{
-    res.render('projects.ejs')
+app.get('/projects', (req, res) => {
+  res.render('projects.ejs')
 })
 
-app.get('/tes', (req, res) =>{
-    io.on('tes', (socket) =>{
-        console.log(socket)
-    })
-    res.render('tes.ejs')
+app.get('/tes', (req, res) => {
+  io.on('tes', (socket) => {
+    console.log(socket)
+  })
+  res.render('tes.ejs')
 })
 
 // API services
 
-app.get('/api/v1/qr', (req, res) =>{
-    (req.query.text == undefined || req.query.text == '') ? res.send({success: false, msg: 'Input salah!!'}) : res.send({success: true, data64: 'data:image/png;base64,' + require('qr-image').imageSync(req.query.text, {type: 'png'}).toString('base64')})
+app.get('/api/v1/qr', (req, res) => {
+  (req.query.text == undefined || req.query.text == '') ? res.send({ success: false, msg: 'Input salah!!' }) : res.send({ success: true, data64: 'data:image/png;base64,' + require('qr-image').imageSync(req.query.text, { type: 'png' }).toString('base64') })
 })
 
-app.get('/api/v1/mal', async(req, res) =>{
-    const mal = (req.query.search == undefined || req.query.search == '') ? {succes: false, msg: 'Bad Request.'} : {success: true, note: 'Data ini di ambil dari: https://www.myanimelist.net', data: await require('axios').get(`https://myanimelist.net/search/prefix.json?type=anime&keyword=${req.query.search}`).then(res => res.data.categories[0].items.map(e =>
-    {
-        return{
-            id: e.id,
-            type: e.payload.media_type,
-            score: e.payload.score,
-            title: e.name,
-            aired: e.payload.aired,
-            url: e.url,
-            thumb: `https://cdn.myanimelist.net/images/${e.image_url.split('/')[6]}/${e.image_url.split('/')[7]}/${e.image_url.split('/')[8]}`,
-        }
-    })).catch(er => er.data)}
-    res.send(mal)
+app.get('/api/v1/mal', async (req, res) => {
+  const mal = (req.query.search == undefined || req.query.search == '') ? { succes: false, msg: 'Bad Request.' } : {
+    success: true, note: 'Data ini di ambil dari: https://www.myanimelist.net', data: await require('axios').get(`https://myanimelist.net/search/prefix.json?type=anime&keyword=${req.query.search}`).then(res => res.data.categories[0].items.map(e => {
+      return {
+        id: e.id,
+        type: e.payload.media_type,
+        score: e.payload.score,
+        title: e.name,
+        aired: e.payload.aired,
+        url: e.url,
+        thumb: `https://cdn.myanimelist.net/images/${e.image_url.split('/')[6]}/${e.image_url.split('/')[7]}/${e.image_url.split('/')[8]}`,
+      }
+    })).catch(er => er.data)
+  }
+  res.send(mal)
 })
 
-app.get('/*', (req, res) =>{
-    res.redirect('/')
+app.get('/*', (req, res) => {
+  res.redirect('/')
 })
 
-server.listen(port, () =>{
-    console.log('Server start on: http://localhost:' + port)
+server.listen(port, () => {
+  console.log('Server start on: http://localhost:' + port)
 })
--- web/root/locJS/tls-script.js
+++ web/root/locJS/tls-script.js
@@ -1,20 +1,33 @@
-$(document).ready(function(){
-    $(".input-group button#qr-btn").click(function() {
-      $.get(`/api/v1/qr?text=${document.querySelector(".input-group input#qr-input").value}`, function(res) {
-          document.querySelector(".input-group input#qr-input").value = document.querySelector(".input-group input#qr-input").defaultValue;
-          res.success == true
-            ? document
-                .querySelector(".cd-ct img")
-                .setAttribute("src", res.data64) ||
-              document
-                .querySelector(".cd-ct a#dl")
-                .classList.remove("disabled") ||
-              document
-                .querySelector(".cd-ct a#dl")
-                .setAttribute("href", res.data64)
-            : document.querySelector(".cd-ct img").setAttribute("src", "null");
-        });
-    });
+$(document).ready(function() {
+  $(".input-group button#qr-btn").click(function() {
+    // bi-arrow-repeat
+    document
+      .querySelector(".input-group button i.bi")
+      .setAttribute("class", "bi bi-arrow-repeat");
+    $.get(`/api/v1/qr?text=${document.querySelector(".input-group input#qr-input").value}`,
+      function(res) {
+        document.querySelector(".input-group input#qr-input").value = document.querySelector(".input-group input#qr-input").defaultValue;
+        res.success == true
+          ? document
+              .querySelector(".cd-ct img")
+              .setAttribute("src", res.data64) ||
+            document
+              .querySelector(".input-group button i.bi")
+              .setAttribute("class", "bi bi-hammer") ||
+            document
+              .querySelector(".cd-ct a#dl")
+              .classList.remove("disabled") ||
+            document
+              .querySelector(".cd-ct a#dl")
+              .setAttribute("href", res.data64)
+          : document.querySelector(".cd-ct img").setAttribute("src", "null") ||
+            document
+              .querySelector(".input-group button i.bi")
+              .setAttribute("class", "bi bi-hammer") ||
+            document.querySelector(".cd-ct a#dl").classList.add("disabled");
+          }
+    );
+  });
 });
 
 // $(document).ready(function(){
@@ -22,7 +35,7 @@ $(document).ready(function(){
 //     const socket = io("localhost:2400");
 //     // feature 1 //
 //     socket.on('QR', (res) =>{(res != false) ? document.querySelector('.cd-ct img').setAttribute('src', `data:image/png;base64,${res}`) : document.querySelector('.cd-ct img').setAttribute('src', 'null');(res != false) ? document.querySelector('.cd-ct a#dl').setAttribute('href', `data:image/png;base64,${res}`) || document.querySelector('.cd-ct a#dl').classList.remove('disabled') : document.querySelector('.cd-ct a#dl').classList.add('disabled'); document.querySelector('.input-group input#qr-input').value = document.querySelector('.input-group input#qr-input').defaultValue;});
-    // $('.input-group button#qr-btn').click(function(){socket.emit('QR', {qr: document.querySelector('.input-group input#qr-input').value});});
+// $('.input-group button#qr-btn').click(function(){socket.emit('QR', {qr: document.querySelector('.input-group input#qr-input').value});});
 //     // feature 2 //
 //     socket.on('MAL', (res) =>{
 //         // const mal_p = document.querySelector('.row div#mal-r');
@@ -32,14 +45,14 @@ $(document).ready(function(){
 //             // ch.map(e =>{
 //             //     console.log(e)
 //             // })
-            
+
 //         }else{
 //             document.querySelector('.row div.mal-data').innerHTML = '<hr><div id="ntf" class="text-muted text-center" style="font-size: 14px;"><i>Input kosong!.</i></div>'
 //         }
 //         $(res).ready(function(){
 //             document.querySelector('.mb-3 input#mal-s').value = document.querySelector('.mb-3 input#mal-s').defaultValue;
 //             document.querySelector('.col button#mal-btn0').classList.toggle('d-none');
-//             document.querySelector('.col button#mal-btn1').classList.toggle('d-none');    
+//             document.querySelector('.col button#mal-btn1').classList.toggle('d-none');
 //         })
 //     })
 //     $('.col button#mal-btn0').click(function(){
