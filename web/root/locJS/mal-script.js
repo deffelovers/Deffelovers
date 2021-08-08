775a00e26c41c779d69bf3e7889120dc3f2c9975
Deffelovers <another.n4krs@gmail.com>
2021-08-08 07:45:13 +0700

    update#7.4

--- web/root/locJS/mal-script.js
+++ web/root/locJS/mal-script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="mal-items"]'))
-    popoverTriggerList.map(function (popoverTriggerEl, i) {
-      const obj_ = JSON.parse(popoverTriggerEl.getAttribute('data-bs-obj'))
-      return new bootstrap.Popover(popoverTriggerEl, { trigger: 'hover', html: true, title: obj_.title, content: `<li><b>Type:</b><i> ${obj_.type}</i></li><li><b>Score Self:</b><i> ${obj_.score}</i></li><li><b>Total Eps:</b><i> ${obj_.eps}</i></li><li><b>On Watch Eps:</b><i> ${obj_.eps_watch}</i></li><li><b>Fin Watch:</b><i> ${obj_.finish_date}</i></li><li><b>End Of Date:</b><i> ${obj_.end_date}</i></li>` })
-    })
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="mal-items"]'))
+  popoverTriggerList.map(function (popoverTriggerEl, i) {
+    const obj_ = JSON.parse(popoverTriggerEl.getAttribute('data-bs-obj'))
+    return new bootstrap.Popover(popoverTriggerEl, { trigger: 'hover', html: true, title: obj_.title, content: `<li><b>Type:</b><i> ${obj_.type}</i></li><li><b>Score Self:</b><i> ${obj_.score}</i></li><li><b>Total Eps:</b><i> ${obj_.eps}</i></li><li><b>On Watch Eps:</b><i> ${obj_.eps_watch}</i></li><li><b>Fin Watch:</b><i> ${obj_.finish_date}</i></li><li><b>End Of Date:</b><i> ${obj_.end_date}</i></li>` })
   })
+})
--- web/root/locJS/tls-script.js
+++ web/root/locJS/tls-script.js
@@ -21,10 +21,14 @@ $(document).ready(function() {
       if(res.success == true){
         let mal_d = ""
         for (let i = 0; i < res.data.length; i++) {
-          mal_d += `<div><div class="mal-item" style="padding: 5px;"><div class="card text-white"><img src="${res.data[i].thumb}" class="card-img" alt="${res.data[i].title}"></div></div><div class="col" style="padding-top: 10px; padding: 5px;"><a href="${res.data[i].url}" target="_blank" class="btn btn-primary" style="width: 100%; font-size: 14px;">Lihat di <b>MAL</b></a></div></div>`;
+          mal_d += `<div id="mal-list"><div class="mal-item" style="padding: 5px;"><div class="card text-white"><img src="${res.data[i].thumb}" class="card-img" alt="${res.data[i].title}" data-bs-toggle="mal-items"></div></div><div class="col" style="padding-top: 10px; padding: 5px;"><a href="${res.data[i].url}" target="_blank" class="btn btn-primary" style="width: 100%; font-size: 14px;">Lihat di <b>MAL</b></a></div></div>`;
         }
         document.querySelector('.card div.mal-data').innerHTML = mal_d
         document.querySelector(".input-group input#mal-input").value = document.querySelector(".input-group input#mal-input").defaultValue; document.querySelector(".input-group button#mal-btn i.bi").setAttribute("class", "bi bi-search");  document.querySelector(".input-group button#mal-btn").classList.remove('disabled')
+        const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="mal-items"]'))
+        popoverTriggerList.map(function (popoverTriggerEl, i) {
+          return new bootstrap.Popover(popoverTriggerEl, { trigger: 'hover', html: true, title: res.data[i].title, content: `<li><b>Type:</b><i> ${res.data[i].type}</i></li><li><b>Score:</b><i> ${res.data[i].score}</i><li><b>Aired:</b><i> ${res.data[i].aired}</i></li>` })
+        })
       }else{
         document.querySelector(".input-group input#mal-input").value = document.querySelector(".input-group input#mal-input").defaultValue;
         document.querySelector('.card div.mal-data').innerHTML = `<i class="text-muted">${res.msg}</i>`; document.querySelector(".input-group button#mal-btn i.bi").setAttribute("class", "bi bi-search"); document.querySelector(".input-group button#mal-btn").classList.remove('disabled');
